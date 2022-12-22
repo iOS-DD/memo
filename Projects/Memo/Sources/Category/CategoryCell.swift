61bbf45efec9c46e017e21980a4cfcb226368a60
JAEYOUNGYUN <lastmore9@gmail.com>
2022-12-22 22:19:15 +0900

    Add unavailable to unimplemented init in CategoryCell

--- Projects/Memo/Sources/Category/CategoryCell.swift
+++ Projects/Memo/Sources/Category/CategoryCell.swift
@@ -40,6 +40,7 @@ final class CategoryCell: UICollectionViewCell {
         setLayout()
     }
 
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
