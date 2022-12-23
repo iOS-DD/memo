3df6c7dfda3e0592dcc4dddf9c528c998f72d719
김지우 <havetherain@gmail.com>
2022-12-23 09:38:25 +0900

    feat: layout 구현  - 작성한 글 목록, 목록 셀 레이아웃 구현

--- Projects/Memo/Sources/ArticleList/ArticleListCell.swift
+++ Projects/Memo/Sources/ArticleList/ArticleListCell.swift
@@ -6,4 +6,93 @@
 //  Copyright © 2022 Memo. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class ArticleListCell: UITableViewCell {
+    static let reuseIdentifier: String = "ArticleListCell"
+
+    private let articleContentLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 14)
+        $0.numberOfLines = 0
+        $0.textAlignment = .center
+        $0.textColor = .darkGray
+    }
+
+    private let sourceStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.distribution = .fill
+        $0.spacing = 4
+    }
+
+    private let additionalInformationStackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.distribution = .fill
+        $0.spacing = 4
+    }
+
+    private let sourceLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 12)
+        $0.textColor = .lightGray
+    }
+
+    private let pageLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 12)
+        $0.textColor = .lightGray
+    }
+
+    private let writerLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 12)
+        $0.textColor = .lightGray
+    }
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        setViewAttributes()
+        render()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setViewAttributes() {
+        backgroundColor = .white
+
+        layer.cornerRadius = 4
+        layer.shadowColor = UIColor.gray.cgColor
+        layer.shadowPath = UIBezierPath(rect: bounds).cgPath
+        layer.shadowOpacity = 0.1
+        layer.shadowRadius = 4
+
+        selectionStyle = .none
+    }
+
+    private func render() {
+        addSubview(articleContentLabel)
+        addSubview(sourceStackView)
+        addSubview(additionalInformationStackView)
+        sourceStackView.addArrangedSubview(sourceLabel)
+        sourceStackView.addArrangedSubview(pageLabel)
+        additionalInformationStackView.addArrangedSubview(sourceStackView)
+        additionalInformationStackView.addArrangedSubview(writerLabel)
+
+        articleContentLabel.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview().inset(16)
+        }
+
+        additionalInformationStackView.snp.makeConstraints {
+            $0.top.equalTo(articleContentLabel.snp.bottom).offset(4)
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.bottom.equalToSuperview().inset(8)
+        }
+    }
+
+    func configure(data: Article) {
+        articleContentLabel.text = data.content
+        sourceLabel.text = data.source
+        pageLabel.text = "\(data.page)"
+        writerLabel.text = data.writer
+    }
+}
--- Projects/Memo/Sources/ArticleList/ArticleListViewController.swift
+++ Projects/Memo/Sources/ArticleList/ArticleListViewController.swift
@@ -13,9 +13,104 @@ import RxSwift
 import SnapKit
 import Then
 
+struct Article {
+    let content: String
+    let source: String
+    let page: Int
+    let writer: String
+}
 
 protocol ArticleListPresentableListener: AnyObject {}
 
 final class ArticleListViewController: UIViewController, ArticleListPresentable, ArticleListViewControllable {
+    private let navigationView = UIView().then {
+        let color: CGFloat = 245 / 255
+        $0.backgroundColor = .init(red: color, green: color, blue: color, alpha: 1)
+    }
+
+    private let titleLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 14, weight: .semibold)
+        $0.text = "내가 담은 글"
+    }
+
+    private let articleCountLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 12)
+        $0.textColor = .darkGray
+    }
+
+    private let tableView = UITableView(frame: .zero, style: .grouped).then {
+        let color: CGFloat = 245 / 255
+        $0.backgroundColor = .init(red: color, green: color, blue: color, alpha: 1)
+        $0.register(ArticleListCell.self, forCellReuseIdentifier: ArticleListCell.reuseIdentifier)
+        $0.sectionHeaderHeight = 12
+        $0.separatorStyle = .none
+    }
+
+    private let articles: [Article] = [
+        .init(content: "첫번째 담은 글 내용", source: "첫번째", page: 1, writer: "홍길동"),
+        .init(content: "두두번째 담은 글 내용", source: "두번째", page: 1, writer: "임꺽정"),
+        .init(content: "세세세번째 담은 글 내용", source: "세번째", page: 1, writer: "장영실"),
+        .init(content: "네네네네번째 담은 글 내용", source: "네번째", page: 1, writer: "이순신"),
+        .init(content: "다섯다섯다섯다섯다섯번째 담은 글 내용", source: "다섯번째", page: 1, writer: "이방원")
+    ]
+
     weak var listener: ArticleListPresentableListener?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setAttributes()
+        render()
+    }
+
+    private func setAttributes() {
+        tableView.dataSource = self
+    }
+
+    private func render() {
+        let color: CGFloat = 245 / 255
+        view.backgroundColor = .init(red: color, green: color, blue: color, alpha: 1)
+
+        view.addSubview(navigationView)
+        view.addSubview(tableView)
+        navigationView.addSubview(titleLabel)
+        navigationView.addSubview(articleCountLabel)
+
+        navigationView.snp.makeConstraints {
+            $0.height.equalTo(48)
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.leading.trailing.equalToSuperview()
+        }
+
+        tableView.snp.makeConstraints {
+            $0.top.equalTo(navigationView.snp.bottom)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+}
+
+extension ArticleListViewController: UITableViewDataSource {
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        return articles.count
+    }
+
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        guard
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: ArticleListCell.reuseIdentifier,
+                for: indexPath
+            ) as? ArticleListCell
+        else {
+            return ArticleListCell()
+        }
+
+        cell.configure(data: articles[indexPath.item])
+        return cell
+    }
 }
